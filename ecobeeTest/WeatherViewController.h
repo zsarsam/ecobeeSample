b3281adf42ef3e65c97cb9923adf78a989ebd1c4
Zaid Sarsam <zaid@clearbridgemobile.com>
2016-01-28 11:16:50 -0500

    adding the weather view controller

---
+++ ecobeeTest/WeatherViewController.h
@@ -0,0 +1,13 @@
+//
+//  WeatherViewController.h
+//  ecobeeTest
+//
+//  Created by Zaid Sarsam on 1/28/16.
+//  Copyright © 2016 sarsam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeatherViewController : UIViewController
+
+@end
